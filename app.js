9e0ca35f3c8297d88de9a7a651bd7ce941ce8fce
HectorDavid09 <101266985+HectorDavid09@users.noreply.github.com>
2022-03-30 09:06:40 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+/* Aplicacion que me guarde los datos en una tabla*/
+
+var nombre;
+var apellido;
+var telefono;
+var email;
+var cumpleaños;
+var direccion;
+var nota;
+
+var tabla = document.getElementById("tabla");
+var boton = document.getElementById("boton");
+
+var contador = 1;
+
+console .log("JavaScript ON!");
